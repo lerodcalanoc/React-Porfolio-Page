46a8084d8930977b9e38c09ebd73de6decf8ac4b
lerodcalanoc <lerodcalanoc@gmail.com>
2019-08-08 20:52:31 -0700

    updated bg

--- src/components/introduction.jsx
+++ src/components/introduction.jsx
@@ -7,7 +7,7 @@ export default class Introduction extends Component {
         <section id="colorlib-hero" className="js-fullheight" data-section="home">
           <div className="flexslider js-fullheight">
             <ul className="slides">
-              <li style={{backgroundImage: 'url(images/me.jpeg)'}}>
+              <li style={{backgroundImage: 'url(images/bg.png)'}}>
                 <div className="overlay"/>
                 <div className="container-fluid">
                   <div className="row">
@@ -22,7 +22,7 @@ export default class Introduction extends Component {
                   </div>
                 </div>
               </li>
-              <li style={{backgroundImage: 'url(images/me.jpeg)'}}>
+              <li style={{backgroundImage: 'url(images/bg.png)'}}>
                 <div className="overlay" />
                 <div className="container-fluid">
                   <div className="row">
--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -8,8 +8,8 @@ export default class Sidebar extends Component {
           <nav href="#navbar" className="js-colorlib-nav-toggle colorlib-nav-toggle" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar"><i /></nav>
           <aside id="colorlib-aside" className="border js-fullheight">
             <div className="text-center">
-              <div className="author-img" style={{backgroundImage: 'url(images/about.jpg)'}} />
-              <h1 id="colorlib-logo"><a href="index.html">Rodel Calanoc</a></h1>
+            <a href="index.html"> <div className="author-img" style={{backgroundImage: 'url(images/about.jpg)'}} /> </a>
+              {/* <h1 id="colorlib-logo"><a href="index.html"> Rodel Calanoc </a></h1> */}
               <span className="email"><i className="icon-mail"></i> <a href="mailto:lerodcalanoc@gmail.com" target="_blank">lerodcalanoc@gmail.com</a>
               </span>
             </div>
